9fd12f482b1a452c3a73540ae01c17e3810984ef
maurnl <mauronluciano@hotmail.com>
2022-11-15 16:49:14 -0300

    agergo tests para Truco

---
+++ ParcialTest/TrucoShould.cs
@@ -0,0 +1,42 @@
+﻿using Biblioteca.Modelos;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcialTest
+{
+    [TestClass]
+    public class TrucoShould
+    {
+        [TestMethod]
+        public void DevolverTipoCorrecto()
+        {
+            // Arrange
+            Truco truco = new Truco();
+
+            // Act
+            IDatosDeJuego<Juego> datosDeJuego = truco.ObtenerDatosDeJuego();
+
+            // Assert
+            Assert.IsInstanceOfType(datosDeJuego, typeof(IDatosDeJuego<Truco>));
+        }
+
+        [TestMethod]
+        public void DevolverDatosDeJuegoNuevos()
+        {
+            // Arrange
+            Truco truco = new Truco();
+
+            // Act
+            IDatosDeJuego<Truco> datosDeJuegoUno = truco.ObtenerDatosDeJuego();
+            IDatosDeJuego<Truco> datosDeJuegoDos = truco.ObtenerDatosDeJuego();
+
+            // Assert
+            Assert.AreNotEqual(datosDeJuegoUno, datosDeJuegoDos);
+        }
+
+    }
+}
